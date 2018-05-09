7c4d9513507cc75ff350296a32fe46c8e418a7a9
rachelnoireau <rachel.noireau@uha.fr>
2018-05-09 12:12:49 +0200

    debut

---
+++ testprojet.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May  7 16:26:16 2018
+
+@author: rachel
+"""
+
+from tkinter import *
+
+fenetre = Tk()
+fenetre.attributes("-fullscreen", 1)
+w=winfo_screenwidth()
+h=winfo_screenheight() 
+#fenetre.configure(width=1000,height=500)
+#Button(fenetre, text="Quit", command=fenetre.destroy).pack()
+fenetre.mainloop()
+
