eed4f56b0ab3dcf3a3f2b68f7b941c5eabefaec0
jungilwoo93 <panpan9311@gmail.com>
2018-05-09 17:38:29 +0200

    panpan fait

--- testprojet.py
+++ testprojet.py
@@ -6,15 +6,21 @@ Created on Mon May  7 16:26:16 2018
 """
 
 import tkinter as tk
-import tkinter.filedialog as tf
-import os
-from os.path import basename
-#from tkFileDialog import *
-from tkinter import *
+#from tkinter import *
 #créer la fenêtre d'application
 fenetre = tk.Tk()
-#mettre le title pour l'application
+#variable
+var = tk.IntVar()
+var.set(1)
+typeZone={"Titre","Paragraphe","Lettrine","Image"}
+
+#fonctions
+def confirmer():
+    print(var)
+    
+#mettre le title et background pour l'application
 fenetre.title("Projet")
+
 #récupérer la taille d'écran d'ordi
 ecran_width = fenetre.winfo_screenwidth()
 ecran_height = fenetre.winfo_screenheight()
@@ -26,63 +32,28 @@ fenetre.geometry(str(ecran_width)+'x'+str(ecran_height))
 #h=fenetre.winfo_screenheight() 
 #fenetre.configure(width=w-2,height=h-50)
 #tk.Button(fenetre, text="Quit", command=fenetre.destroy).pack()
+
+#frame pour le zone avec les radiobuttons
+f2=tk.Frame(fenetre)
+#label pour le zone choisit
+labelZoneChoix=tk.Label(f2,text='La zone choisit est : ')
+labelZoneChoix.config(font=('Forte',18))
+labelZoneChoix.grid(row=0, sticky=W)
+#les radiobuttons pour les choix
+a=1
+for i,v in enumerate(typeZone):
+    print(i)
+    tk.Radiobutton(f2, text=v, variable=var, value = a).grid(row=a, column=1,sticky=W)
+    a+=1
+
+#button pour comfirmer le choix avec la zone choisit sur image
+buttonConfirm=tk.Button(f2,text="Confirmer",command=confirmer).grid(row=len(typeZone)+1,column=1,sticky=W)
+
+f2.pack()
+f2.grid(row=1,column=2)
+
+
+
 #démarrer du réceptionnaire d'événements
-
-
-
-
-listFiles=tk.Listbox()
-
-
-
-
-#listbox = tk.Listbox(fenetre, yscrollcommand=scrollbar.set)
-#scrollbar = tk.Scrollbar(listbox)
-#scrollbar.pack(side='right', fill='y')
-#listbox.pack()
-
-#scrollbar.config(command=listbox.yview)
-#defilX = tk.Scrollbar(self, orient='horizontal',command=listFiles.xview)
-#defilX.grid(row=1, column=0, sticky='ew')
-#listFiles['xscrollcommand']=defilX.set
-i=1;
-#parcours choit du fichier
-def chooseFile():
-    #choice=fenetre.FileDialog(tf.msoFileDialogOpen)
-    choice = tf.askopenfilenames() #fichier uniquement
-    #choice = tf.askdirectory()  #repertoire uniquement
-    #defaultextension='.png'
-    #print(choice)
-    #choiceBoth=tf.
-    #os.listdir #pour recupereelement d'un dosier
-    
-    nbSelected=len(choice)
-    #recupere le nom apartir du chemin
-    for i in range (0,nbSelected):
-        nom=basename(choice[i])
-        if nom!="": #sinon quand on clic sur parcourir mais qu'on ne choisi rien ça rajoute un espace blanc
-                listFiles.insert(1,nom) #ça se met par ordre aleatoire
-                #i+=1
-                #print(i)
-
-#supprimer de la liste les fichiers selectionnés
-def delecteSelection():
-    selection = listFiles.curselection()
-    listFiles.delete(selection[0])    
- 
-#supprimer de la liste tout les fichiers
-def delecteAll():
-    #cs=listFiles.curselection()
-    #listFiles.delete(0,cs[0] -1)
-    listFiles.delete(0,tk.END)
-    
-    
-#listFiles.pack(side="left",fill="y")    
-boutonParcourir=tk.Button(fenetre,text="parcourir",command=chooseFile).pack()
-boutonSupprimer=tk.Button(fenetre,text="supprimer",command=delecteSelection).pack()
-boutonSupprimer=tk.Button(fenetre,text="vider",command=delecteAll).pack()
-
-
-
 fenetre.mainloop()
 
