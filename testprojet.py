ebfa84c704cacc52d034d2b38de74ec2318ec6a2
rachelnoireau <rachel.noireau@uha.fr>
2018-05-09 17:34:10 +0200

    scollbar en cours

--- testprojet.py
+++ testprojet.py
@@ -10,7 +10,7 @@ import tkinter.filedialog as tf
 import os
 from os.path import basename
 #from tkFileDialog import *
-#from tkinter import *
+from tkinter import *
 #créer la fenêtre d'application
 fenetre = tk.Tk()
 #mettre le title pour l'application
@@ -28,30 +28,56 @@ fenetre.geometry(str(ecran_width)+'x'+str(ecran_height))
 #tk.Button(fenetre, text="Quit", command=fenetre.destroy).pack()
 #démarrer du réceptionnaire d'événements
 
+
+
+
 listFiles=tk.Listbox()
+
+
+
+
+#listbox = tk.Listbox(fenetre, yscrollcommand=scrollbar.set)
+#scrollbar = tk.Scrollbar(listbox)
+#scrollbar.pack(side='right', fill='y')
+#listbox.pack()
+
+#scrollbar.config(command=listbox.yview)
+#defilX = tk.Scrollbar(self, orient='horizontal',command=listFiles.xview)
+#defilX.grid(row=1, column=0, sticky='ew')
+#listFiles['xscrollcommand']=defilX.set
 i=1;
 #parcours choit du fichier
 def chooseFile():
-    choice = tf.askopenfilename()
-    nom=basename(choice)
-    print(nom)
-    if nom!="":
-            listFiles.insert(1,nom) #ça se met par ordre alphabetique
+    #choice=fenetre.FileDialog(tf.msoFileDialogOpen)
+    choice = tf.askopenfilenames() #fichier uniquement
+    #choice = tf.askdirectory()  #repertoire uniquement
+    #defaultextension='.png'
+    #print(choice)
+    #choiceBoth=tf.
+    #os.listdir #pour recupereelement d'un dosier
     
-    #i+=1
-    #print(i)
+    nbSelected=len(choice)
+    #recupere le nom apartir du chemin
+    for i in range (0,nbSelected):
+        nom=basename(choice[i])
+        if nom!="": #sinon quand on clic sur parcourir mais qu'on ne choisi rien ça rajoute un espace blanc
+                listFiles.insert(1,nom) #ça se met par ordre aleatoire
+                #i+=1
+                #print(i)
 
+#supprimer de la liste les fichiers selectionnés
 def delecteSelection():
     selection = listFiles.curselection()
     listFiles.delete(selection[0])    
-    
+ 
+#supprimer de la liste tout les fichiers
 def delecteAll():
-    cs=listFiles.curselection()
+    #cs=listFiles.curselection()
     #listFiles.delete(0,cs[0] -1)
     listFiles.delete(0,tk.END)
     
     
-listFiles.pack()    
+#listFiles.pack(side="left",fill="y")    
 boutonParcourir=tk.Button(fenetre,text="parcourir",command=chooseFile).pack()
 boutonSupprimer=tk.Button(fenetre,text="supprimer",command=delecteSelection).pack()
 boutonSupprimer=tk.Button(fenetre,text="vider",command=delecteAll).pack()
