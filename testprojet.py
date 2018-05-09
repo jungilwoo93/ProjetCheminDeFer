b4446c08e746350b112287d92fed0d77f5f96098
jungilwoo93 <panpan9311@gmail.com>
2018-05-09 13:51:08 +0200

    fenetre a taille d'ecran d'ordi

--- testprojet.py
+++ testprojet.py
@@ -2,16 +2,26 @@
 """
 Created on Mon May  7 16:26:16 2018
 
-@author: rachel
+@author: Rachel Noireau et Liuyan PAN
 """
 
-from tkinter import *
-
-fenetre = Tk()
-fenetre.attributes("-fullscreen", 1)
-w=fenetre.winfo_screenwidth()
-h=fenetre.winfo_screenheight() 
-fenetre.configure(width=w-2,height=h-50)
-Button(fenetre, text="Quit", command=fenetre.destroy).pack()
+import tkinter as tk
+#from tkinter import *
+#créer la fenêtre d'application
+fenetre = tk.Tk()
+#mettre le title pour l'application
+fenetre.title("Projet")
+#récupérer la taille d'écran d'ordi
+ecran_width = fenetre.winfo_screenwidth()
+ecran_height = fenetre.winfo_screenheight()
+#définir la taille d'écran d'or comme la fenêtre d'application
+fenetre.geometry(str(ecran_width)+'x'+str(ecran_height))
+#rachel a fait
+#fenetre.attributes("-fullscreen", 1)
+#w=fenetre.winfo_screenwidth()
+#h=fenetre.winfo_screenheight() 
+#fenetre.configure(width=w-2,height=h-50)
+tk.Button(fenetre, text="Quit", command=fenetre.destroy).pack()
+#démarrer du réceptionnaire d'événements
 fenetre.mainloop()
 
