192a71e0f8b09cfb8b73be5d550573add2a17873
jungilwoo93 <panpan9311@gmail.com>
2018-05-11 11:55:51 +0200

    ajouter ce que rachel a fait pour select fichier, afficher les photos

--- testprojet.py
+++ testprojet.py
@@ -7,6 +7,7 @@ Created on Mon May  7 16:26:16 2018
 
 import tkinter as tk
 import tkinter.filedialog as tf
+from PIL import Image, ImageFont, ImageDraw, ImageTk
 #import os
 from os.path import basename
 #from tkinter import *
@@ -38,8 +39,26 @@ f1=tk.Frame(fenetre)
 labelFichier=tk.Label(f1,text='Les fichier choisit : ')
 labelFichier.config(font=('Forte',18))
 labelFichier.grid(row=0,sticky=tk.W,pady=5)
+
 listFrame=tk.Frame(f1)
-listFiles=tk.Listbox(listFrame,width=70,height=15,selectmode=tk.SINGLE).pack(pady=10,padx=20)
+yDefilB = tk.Scrollbar(f1, orient='vertical')
+yDefilB.grid(row=0, column=1, sticky='ns')
+
+xDefilB = tk.Scrollbar(f1, orient='horizontal')
+xDefilB.grid(row=1, column=0, sticky='ew')
+
+listFiles = tk.Listbox(listFrame,
+     xscrollcommand=xDefilB.set,
+     yscrollcommand=yDefilB.set,width=70,height=15,selectmode=tk.SINGLE)
+listFiles.pack(pady=10,padx=20)#'nsew'
+xDefilB['command'] = listFiles.xview
+yDefilB['command'] = listFiles.yview
+#scrollbar.config(command=listbox.yview)
+#defilX = tk.Scrollbar(self, orient='horizontal',command=listFiles.xview)
+#defilX.grid(row=1, column=0, sticky='ew')
+#listFiles['xscrollcommand']=defilX.set
+j=1;#i=1
+#listFiles=tk.Listbox(listFrame,width=70,height=15,selectmode=tk.SINGLE).pack(pady=10,padx=20)
 listFrame.grid(row=1,sticky=tk.W)
 
 #listbox = tk.Listbox(fenetre, yscrollcommand=scrollbar.set)
@@ -70,7 +89,14 @@ def chooseFile():
                 listFiles.insert(1,nom) #ça se met par ordre aleatoire
                 #i+=1
                 #print(i)
-
+    listFiles.Sorted = True
+    while i < (listFiles.size() - 1) :
+        if (listFiles.get(i + 1,i+1) == listFiles.get(i,i)) :
+            listFiles.delete(i,i)
+            i = 0
+        else :
+            i += 1
+            
 #supprimer de la liste les fichiers selectionnés
 def delecteSelection():
     selection = listFiles.curselection()
@@ -113,6 +139,24 @@ listAction = tk.Listbox(f2,width=70,height=10)
 listAction.grid(row=len(typeZone)+3,column=0)
 f2.grid(row=2,column=1)
 
+#afficher image dés qu'on selectionner un element
+def onselect(evt):
+    print("element selectionner")
+    cadre=tk.Canvas(fenetre,width=440,height=380)#,bg="black"
+    dicimg = {}
+    #selection = listFiles.curselection()
+    #print(selection[0])
+    w=evt.widget
+    index = int(w.curselection()[0])
+    value = w.get(index)
+    img=Image.open(value)
+    photo = ImageTk.PhotoImage(img)
+    dicimg['img1'] = photo
+    cadre.image=photo
+    item = cadre.create_image(320,240,image =photo) 
+    cadre.grid(row=1,column=2)
+
+listFiles.bind('<<ListboxSelect>>', onselect)   
 #démarrer du réceptionnaire d'événements
 fenetre.mainloop()
 
