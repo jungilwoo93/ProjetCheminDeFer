da5b8259ba7ca21af0662d6c12459c7991ab8638
rachelnoireau <rachel.noireau@uha.fr>
2018-05-15 20:02:30 +0200

    xml rectification

--- xml.py
+++ xml.py
@@ -9,18 +9,45 @@ import os.path
 #from xml.dom import minidom
 
 #C:/Users/rachel/Documents/GitHub/ProjetCheminDeFer/docXml/
-xmlProjet = etree.Element('newProject')
-numero=0
+xmlProjets
+
+try :
+    xmlProjets[0][0]
+except IOError :
+    xmlProjets = [[],[]]
+#name=[]
+#memoire=[]
+#xmlProjet.append(name)
+#xmlProjet.append(memoire)
+xmlProjet
+numero
 
 def checkFileExiste(nameFile):
     #nameFile.exists()
     return os.path.exists(nameFile)
 
+
 def newProjet(nameProjet):
+    #if os.path.exists(nameProjet):
+    print('ce nom est déjà prit')
+    #demande de changer
+    #else:
     global xmlProjet
-    xmlProjet = etree.Element(nameProjet)
+    xmlProjet = etree.Element(nameProjet)#fait recommencer
+    global xmlProjets
+    #size=len(xmlProjets)
+    #xmlProjets[0][size]=nameProjet
+    #xmlProjets[1][size]=xmlProjet
+    xmlProjets[0].append(nameProjet)
+    xmlProjets[1].append(xmlProjet)
     global numero 
     numero=0     
+
+def continuePoject(nameProjet):
+    global xmlProjet
+    #xmlProjet= etree.Element(nameProjet)
+    index=xmlProjets[0].index(nameProjet)
+    xmlProjet=xmlProjets[1][index]
 
 def newPage(pathPage):
     global numero 
@@ -37,9 +64,11 @@ def newPage(pathPage):
 def endProjet(nameProjet) :   
     #xmlProjet = etree.Element(nameProjet)
     try:
-        with open(nameProjet +'.xml','w') as fichier:
+        with open('docXml/' + nameProjet +'.xml','w') as fichier:
         #En-tête du fichier xml
             fichier.write('<?xml version="1.0" encoding="UTF_8"?>\n')
+            index=xmlProjets[0].index(nameProjet)
+            xmlProjets[1][index]=xmlProjet
             fichier.write(etree.tostring(xmlProjet,pretty_print=True).decode('utf-8'))#cree premiere balise
             #xmlProjet = etree.Element(nameProjet)
     except IOError:
@@ -74,8 +103,14 @@ def ajouterElement(typeEl, posiX, posiY, widthEl, heightEl,page):
     height.text= str(heightEl)
 
 
-
-newProjet('myNewProjet')
-p=newPage('mynewPage')
+#def delectElement
+    
+nameProjet='myNewProjet'
+#newProjet(nameProjet)
+continuePoject(nameProjet)
+p=newPage('page original me revoila')
 ajouterElement('para',1,2,3,4,p)
-endProjet('new')
+#ajouterElement('paruhjkl',7,2,3,4,p)
+endProjet(nameProjet)
+ajouterElement('paruhjkl',7,2,3,4,p)
+endProjet(nameProjet)
