afcb9db7a6df7394658fa843215d2d17bca083d5
jungilwoo93 <panpan9311@gmail.com>
2018-05-15 13:00:47 +0200

    re organise les codes et essaie de regler le probleme d'organisation des objets

--- testprojet.py
+++ testprojet.py
@@ -10,7 +10,7 @@ import tkinter.filedialog as tf
 from PIL import Image, ImageFont, ImageDraw, ImageTk
 #import os
 from os.path import basename
-
+#########################################################  fenetre principale ##################################################
 #créer la fenêtre d'application
 root = tk.Tk()
 #récupérer la taille d'écran d'ordi
@@ -18,12 +18,9 @@ ecran_width = root.winfo_screenwidth()
 ecran_height = root.winfo_screenheight()-50
 #définir la taille d'écran d'or comme la fenêtre d'application
 root.geometry(str(ecran_width)+'x'+str(ecran_height))
-
-
-f1=tk.Frame(root,bg='gold', width=ecran_width+1000, height=ecran_height)
-f1.config(width=ecran_width+1000, height=ecran_height)
-f1.grid(column=0,columnspan=1000,sticky=tk.E)
-
+#mettre le title et background pour l'application
+root.title("Projet")
+#scrollbar pour la fenetre pricipqle
 vsb = tk.Scrollbar(root, orient=tk.VERTICAL)
 vsb.grid(row=0, column=3, rowspan=4, sticky=tk.N+tk.S+tk.E)#
 hsb = tk.Scrollbar(root, orient=tk.HORIZONTAL)
@@ -37,50 +34,18 @@ hsb.config(command=c.xview)
 root.grid_rowconfigure(0, weight=1)
 root.grid_columnconfigure(0, weight=1)
 
-#variable global
+######################################################## variable global #######################################################
 var=tk.StringVar()
 var.set("Paragraphe")
 typeZone={"Titre","Paragraphe","Lettrine","Image"}
-
-#les fonctions
-#fonctions de buttonConfirm
-def confirmer():
-    listAction.insert(tk.END,var.get())
-
-    
-#mettre le title et background pour l'application
-root.title("Projet")
-
-
-
-#creer un nouveau frame pour la partie de fichier
-#f1=tk.Frame(root)
-labelFichier=tk.Label(f1,text='Les fichiers choisis : ')
-labelFichier.config(font=('Forte',18))
-labelFichier.grid(row=0,sticky=tk.W,pady=5)
-
-listFrame=tk.Frame(f1)
-yDefilB = tk.Scrollbar(listFrame, orient='vertical')
-yDefilB.grid(row=0, column=1, sticky='ns')
-
-xDefilB = tk.Scrollbar(listFrame, orient='horizontal')
-xDefilB.grid(row=1, column=0, sticky='ew')
-
-listFiles = tk.Listbox(listFrame,
-     xscrollcommand=xDefilB.set,
-     yscrollcommand=yDefilB.set,width=70,height=15,selectmode=tk.SINGLE)
-listFiles.grid(row=0)#'nsew'
-xDefilB['command'] = listFiles.xview
-yDefilB['command'] = listFiles.yview
-
-
-listFrame.grid(row=1,pady=5,padx=20,sticky=tk.W)
-
-
-
 listPath=[]
 
-#parcours choit du fichier
+#################################################### toutes les fonctions  ####################################################
+# fonction de buttonConfirm
+def confirmer():
+    listAction.insert(tk.END,var.get())
+    
+# parcours choit du fichier
 def chooseFile():
     #choice=fenetre.FileDialog(tf.msoFileDialogOpen)
     choice = tf.askopenfilenames() #fichier uniquement
@@ -114,55 +79,81 @@ def chooseFile():
             j += 1
     listPath.reverse()
             
-#supprimer de la liste les fichiers selectionnés
+# supprimer de la liste les fichiers selectionnés
 def delecteSelection():
     selection = listFiles.curselection()
     listFiles.delete(selection[0])
-    listPath.remove(selection[0])####################################
+    listPath.remove(selection[0])
  
-#supprimer de la liste tout les fichiers
+# supprimer de la liste tout les fichiers
 def delecteAll():
     #cs=listFiles.curselection()
     #listFiles.delete(0,cs[0] -1)
     listFiles.delete(0,tk.END) 
     listPath.clear()
-    
-#listFiles.pack(side="left",fill="y")   
+
+############################################################ frame à gauche ####################################################
+#f1=tk.Frame(root,bg='gold', width=ecran_width+1000, height=ecran_height)
+#f1.config(width=ecran_width+1000, height=ecran_height)
+#f1.grid(column=0,columnspan=1000,sticky=tk.E)
+f1=tk.Frame(root,bg='gold')
+
+####################### label fichiers choisis
+labelFichier=tk.Label(f1,text='Les fichiers choisis : ')
+labelFichier.config(font=('Forte',18))
+labelFichier.grid(row=0,sticky=tk.W,pady=5)
+
+################################ frame pour listeBox et leur scrollbar
+listFrame=tk.Frame(f1)
+
+###################### scrollbar vertical et horizontal
+yDefilB = tk.Scrollbar(listFrame, orient='vertical')
+yDefilB.grid(row=0, column=1, sticky='ns')
+xDefilB = tk.Scrollbar(listFrame, orient='horizontal')
+xDefilB.grid(row=1, column=0, sticky='ew')
+
+##################### listBox pour afficher les fichiers choisis
+listFiles = tk.Listbox(listFrame,
+     xscrollcommand=xDefilB.set,
+     yscrollcommand=yDefilB.set,width=70,height=15,selectmode=tk.SINGLE)
+listFiles.grid(row=0)#'nsew'
+#listFiles.pack(side="left",fill="y")  
+xDefilB['command'] = listFiles.xview
+yDefilB['command'] = listFiles.yview
+listFrame.grid(row=1,pady=5,padx=20,sticky=tk.W)
+
+################### frame pour les buttons parcourir, supprimer, vider
 zoneButton=tk.Frame(f1)
 boutonParcourir=tk.Button(zoneButton,text="Parcourir",command=chooseFile).grid(row=1, column=0,sticky=tk.S,padx=40)
 boutonSupprimer=tk.Button(zoneButton,text="Supprimer",command=delecteSelection).grid(row=1, column=1,sticky=tk.S,padx=40)
 boutonSupprimer=tk.Button(zoneButton,text="Vider",command=delecteAll).grid(row=1, column=2,sticky=tk.S,ipadx=10,padx=40)
 zoneButton.grid(row=2,pady=5)
-#f1.grid(row=1,column=1)
 
-#nouveau frame pour le zone avec les radiobuttons
-#f2=tk.Frame(fenetre)
-#label pour le zone choisit
+################## label pour la zone choisie
 labelZoneChoix=tk.Label(f1,text='La zone choisi est : ')
 labelZoneChoix.config(font=('Forte',18))
 labelZoneChoix.grid(row=3, sticky=tk.W)
 
-#les radiobuttons pour les choix
+########################3 frame pour afficher les radiobuttons des choix
 zoneRadioButton=tk.Frame(f1)
 a=0
 for i,v in enumerate(typeZone):
     tk.Radiobutton(zoneRadioButton, text=v, variable=var, value = v).grid(row=a, column=0,sticky=tk.W)
     a+=1
 zoneRadioButton.grid(row=4,sticky=tk.W,pady=5)
-#button pour comfirmer le choix avec la zone choisit sur image
+
+################ button pour comfirmer le choix avec la zone choisit sur image
 buttonConfirm=tk.Button(f1,text="Confirmer",command=confirmer).grid(row=5,column=0,pady=5,sticky=tk.S)
-#liste Action
+
+################ listebox pour les Actions
 labelAction=tk.Label(f1,text="Les actions : ")
 labelAction.config(font=('Forte',18))
 labelAction.grid(row=6,column=0,pady=5,sticky=tk.W)
 listAction = tk.Listbox(f1,width=70,height=8)
 listAction.grid(row=7,column=0,pady=5)
-f1.grid(row=0,column=1)
+f1.grid(row=0,column=0)
 
-
-
-
-
+############################################### frame à droite pour afficher l'image ##########################################
 #f0 = tk.Frame(f1)
 #f0.grid(fill=tk.BOTH)
 #f0.pack(fill=tk.BOTH)
@@ -174,12 +165,12 @@ f1.grid(row=0,column=1)
 #f0.update_idletasks()
 #c.config(scrollregion=c.bbox("all"))#a mettre a la fin
 
-#afficher image dés qu'on selectionner un element
-zoneImage=tk.Frame(f1,bg="black")
+#afficher image dés qu'on selectionne un element
+#zoneImage=tk.Frame(root,bg="black")
 #zoneImage.grid(row=2,column=10,rowspan=2,columnspan=8,sticky=tk.E )
 
 def onselect(evt):
-    cadre=tk.Canvas(zoneImage,yscrollcommand=vsb.set, xscrollcommand=hsb.set,width=320,height=240,bg="black")#,bg="black"
+    cadre=tk.Canvas(root,yscrollcommand=vsb.set, xscrollcommand=hsb.set,width=320,height=240,bg="black")#,bg="black"
     dicimg = {}
     #selection = listFiles.curselection()
     #print(selection[0])
@@ -196,8 +187,9 @@ def onselect(evt):
         
         cadre.create_image(400,320,image =photo) 
         #zoneImage.grid(row=2,column=5000,rowspan=2,columnspan=8,sticky=tk.E)#,padx=20,pady=20
-        cadre.grid(row=2,column=500,rowspan=2,columnspan=30,sticky=tk.E)# padx=20,pady=20,
-        zoneImage.grid(row=2,column=10,rowspan=2,columnspan=8,sticky=tk.E )
+        #cadre.grid(row=2,column=500,rowspan=2,columnspan=30,sticky=tk.E)# padx=20,pady=20,
+        cadre.grid(row=0,column=1,sticky=tk.W)
+        #zoneImage.grid(row=0,column=1,rowspan=2,columnspan=8,sticky=tk.E )
         cadre.config(scrollregion=cadre.bbox("all"))
 
 listFiles.bind('<<ListboxSelect>>', onselect)  
