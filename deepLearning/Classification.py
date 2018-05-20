d47d77debac42c0d39bf2556f041af9b4afba623
rachelnoireau <rachel.noireau@uha.fr>
2018-05-20 19:28:11 +0200

    deep learnind se que les autres on fait

---
+++ deepLearning/Classification.py
@@ -0,0 +1,152 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  5 01:37:20 2018
+
+@author: DL9
+"""
+import glob
+from sklearn import preprocessing
+from sklearn.preprocessing import StandardScaler
+from sklearn.cross_validation import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+import xml.etree.ElementTree as ET
+import pandas as pd
+
+
+
+
+
+dataSet=[] #training dataset
+unknownSet=[]
+
+
+class Component(object):
+
+    def __init__(self,tp,x,y,w,h):
+        self.type=tp
+        self.x=x
+        self.y=y
+        self.w=w
+        self.h=h
+        
+
+def extractPaths(path):
+    xmlFiles=glob.glob(path+"/*.xml")
+    fileList=list()
+    for x in range(0,len(xmlFiles)):
+         fileList.append(xmlFiles[x][39:])
+    return fileList
+    
+def extractUnlabelledPaths(path):
+    xmlFiles=glob.glob(path+"/*.xml")
+    fileList=list()
+    for x in range(0,len(xmlFiles)):
+         fileList.append(xmlFiles[x][75:])
+    return fileList
+    
+
+
+
+def feedList(tp,x,y,w,h): #type,point x, pointY, RectangleWidth,RectangleHeight
+   
+    if tp == "Paragraphe":
+        dataSet.append([x,y,w,h,1])
+        
+    elif tp == "Titre":
+         dataSet.append([x,y,w,h,2])
+    
+def feedUnlabelledList(tp,x,y,w,h,i):
+   
+    if tp == "unknown":
+        unknownSet.append([x,y,w,h,i])
+
+def extractUnlabelledData(path):
+    fileNames=extractUnlabelledPaths(path)
+    for x in range(0,len(fileNames)):
+        tree = ET.parse(path+''+fileNames[x])
+        root = tree.getroot()
+        for component in root.iter('component'):
+            feedUnlabelledList(component.find('type').text,component.find('PointX').text,component.find('PointY').text,component.find('RectangleWidth').text,component.find('RectangleHeight').text,fileNames[x])
+
+def extractData(path):
+    fileNames=extractPaths(path)
+    for x in range(0,len(fileNames)):
+        tree = ET.parse(path+''+fileNames[x])
+        root = tree.getroot()
+        for component in root.iter('component'):
+            feedList(component.find('type').text,component.find('PointX').text,component.find('PointY').text,component.find('RectangleWidth').text,component.find('RectangleHeight').text)
+
+def rewriteXml():
+   if len(unknownSet)!=0:
+       
+       for x in range(0,len(unknownSet)):
+                tree = ET.parse('C:/Users/DL9/Desktop/Machine Learning/Projet3A/Draw on image/workshop_test/'+unknownSet[x][4])
+                root = tree.getroot()
+                for component in root.iter('component'):
+                    
+                    if component.find('type').text=="unknown":
+                        print(x)
+                        if y_pred[x]==1:
+                            component.find('type').text="Paragraphe"
+                        else:
+                            component.find('type').text="Titre"
+                        tree.write('C:/Users/DL9/Desktop/Machine Learning/Projet3A/Draw on image/workshop_test/'+unknownSet[x][4])
+                        break
+                        
+                    
+
+                  
+
+extractData('C:/Users/DL9/Desktop/XML-Training-Data/')
+
+extractUnlabelledData("C:/Users/DL9/Desktop/Machine Learning/Projet3A/Draw on image/workshop_test/")
+
+
+'''transforming our dataset into a pandaDataFrame'''
+
+pdDataSet = pd.DataFrame(dataSet)
+pdUnlabelledData=pd.DataFrame(unknownSet)
+
+'''test train split 25% 75%'''
+
+x_train,x_test,y_train,y_test=train_test_split(pdDataSet.iloc[:,[0,3]].values ,pdDataSet.iloc[:,[4]].values,test_size=0.25,random_state=0)
+
+scaler = StandardScaler()
+
+x_train = scaler.fit_transform(x_train)
+x_test = scaler.transform(x_test)
+
+classifier=KNeighborsClassifier(n_neighbors=3,metric='minkowski',p=2)
+classifier.fit(x_train,y_train)
+''' testing our knn on the training data'''
+y_pred=classifier.predict(x_test)
+
+
+from sklearn.metrics import confusion_matrix
+cm=confusion_matrix(y_test,y_pred) #la confusion_matrix permet de calculer l'erreur, mais il existe d'autre methode pour la calculer 
+
+
+
+
+''' classifying unlabelled data from xml files'''
+x_train=pdDataSet.iloc[:,[0,3]].values
+y_train=pdDataSet.iloc[:,[4]].values
+
+x_test=pdUnlabelledData.iloc[:,[0,3]].values
+
+
+
+x_train = scaler.fit_transform(x_train)
+x_test = scaler.transform(x_test)
+
+classifier=KNeighborsClassifier(n_neighbors=3,metric='minkowski',p=2)
+classifier.fit(x_train,y_train)
+'''real classification'''
+y_pred=classifier.predict(x_test)
+'''rewriting the xml files after the classification of the unknown components'''
+rewriteXml()
+
+
+
+
+
---
+++ deepLearning/Segmentation.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 19 14:04:46 2018
+
+@author: DL9
+"""
+
+import cv2
+import xml.etree.cElementTree as ET
+
+for x in range(130, 169):  # les images chargées pour la segmentation image130 à image 169
+    id_img="page_n-"+str(x)+".png"
+    
+    
+    root = ET.Element('Image',id=id_img)
+    components = ET.SubElement(root, "Components")
+    
+    
+    
+    or_im=cv2.imread("imgFromPdf/"+id_img)  #les images extraite en haute qualité se trouve dans le dossier /images
+    kernel1 = cv2.getStructuringElement(cv2.MORPH_CROSS, (1, 2)) 
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 1))
+    
+    #Read a grayscale image
+    im_gray = cv2.imread("imgFromPdf/"+id_img, cv2.IMREAD_GRAYSCALE)
+    
+    #Convert grayscale image to binary
+    (thresh, im_bw) = cv2.threshold(im_gray, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+    
+    #inverting the colors since opencv uses black as a background
+    im_bw=cv2.bitwise_not(im_bw)
+    im_bw1=im_bw
+    
+    im_bw=cv2.erode(im_bw,kernel, iterations=1)
+    im_bw = cv2.dilate(im_bw,kernel1, iterations=2)
+    
+    #finding lettrines
+    _, contours, _=  cv2.findContours(im_bw, cv2.RETR_LIST,
+    	cv2.CHAIN_APPROX_NONE)
+    
+    for contour in contours:
+             [x, y, w, h] = cv2.boundingRect(contour)
+             if w <500 and h <50 :    #finding the big bigblocks aka "paragraphes"
+    
+             component = ET.SubElement(components, "component")
+             ET.SubElement(component, "type").text = "lettrine"
+             ET.SubElement(component, "RectangleWidth").text = str(w)
+             ET.SubElement(component, "RectangleHeight").text = str(h)
+             ET.SubElement(component, "PointX").text = str(x)
+             ET.SubElement(component, "PointY").text = str(y)
+             
+             cv2.rectangle(or_im, (x, y), (x + w, y + h), (255, 0, 0), 3)
+             cv2.putText(or_im, "letr", (x - 20, y - 20),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
+             
+    im_bw1 = cv2.dilate(im_bw1,kernel, iterations=5)   
+    
+    
+    kernel2 = cv2.getStructuringElement(cv2.MORPH_RECT, (7, 7))
+    im_bw1 = cv2.morphologyEx(im_bw1, cv2.MORPH_CLOSE, kernel2)
+    _, contours1, _=  cv2.findContours(im_bw1.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    
+    #finding bigblocks
+    for contour in contours1:
+             [x, y, w, h] = cv2.boundingRect(contour)
+             if w <80 and h <40 :  #les lettrines en generales sans assez petites
+                 continue
+    
+             component = ET.SubElement(components, "component")
+             ET.SubElement(component, "type").text = "unknown"
+             ET.SubElement(component, "RectangleWidth").text = str(w)
+             ET.SubElement(component, "RectangleHeight").text = str(h)
+             ET.SubElement(component, "PointX").text = str(x)
+             ET.SubElement(component, "PointY").text = str(y)
+             cv2.rectangle(or_im, (x, y), (x + w, y + h), (0, 255, 0), 1)
+             cv2.putText(or_im, "txt", (x - 20, y - 20),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)   #cette ligne permet de dessiner sur l'image segmenter pour verifier la segmentation, cela est optionnel
+    
+    #saving
+    tree = ET.ElementTree(root)
+    tree.write("Results/xml_results/"+id_img+"-Unlabelled.xml")     #writing elements to an unlabelled xml(it's unlabelled since we have not classified it yet)
+    cv2.imwrite('bw_image.png', im_bw1)        #optionnel visuel d'une image en "mi-processing"
+    cv2.imwrite("Results/"+id_img, or_im)      #optionnel image segmenter
