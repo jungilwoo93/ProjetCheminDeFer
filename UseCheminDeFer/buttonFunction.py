0c1874ce07041ee61fbcbb7cea8e8c5395f29e40
rachelnoireau <rachel.noireau@uha.fr>
2018-06-14 19:25:37 +0200

    commentaire

--- UseCheminDeFer/buttonFunction.py
+++ UseCheminDeFer/buttonFunction.py
@@ -12,41 +12,32 @@ from PIL import Image, ImageFont, ImageDraw, ImageTk
 from UseCheminDeFer import zoomImag as zi
 import time
 
+#nos variable
 listImgOfCanvas=[]
 zoomImg=None
 
-# def ChooseWhereSave():
-	# f=tkinter.filedialog.asksaveasfile(
-		# title="Enregistrer sous … un fichier",
-		# filetypes=[('PDF files','.pdf')])
-	# print(f.name) 
-	
 
 
 
-
-
+#inverse l'etat des rectangle entre plein et vide
 def fullRect(full):
 	return not(full)
 
 
-
-
+#retourne la liste des images
 def getListImg(nameProjet,rectFull):
 	if rectFull:
 		rect='fullRect'
 	else:
 		rect='emptyRect'
 	listImg = os.listdir('DrawOnImage/finalResult/' + nameProjet + '/' +rect)
-	#chrono = lambda v: os.path.getmtime(os.path.join('DrawOnImage/finalResult/' + nameProjet + '/' +rect, v))
-	#listImg.sort(key = chrono)
 	size = len(listImg)
 	listImgOrder=[]
 	for i in range (0,size):
 		listImgOrder.append('classified-page-' + str(i) + '.png')
 	return listImgOrder
 	
-	
+#met a jour le canvas	
 def setCanvas(canva,dicimg,listImg,mwd,mhg,dm,nameProjet,rectFull,newWin):
 	global zoomImg
 	if rectFull:
@@ -74,28 +65,6 @@ def setCanvas(canva,dicimg,listImg,mwd,mhg,dm,nameProjet,rectFull,newWin):
 		if posX>=(wd*widthImg) :
 			posY+=hg+spacing
 			posX=0
-	#posX=0
-	#posY=0
-	#scale= 1.0*wd/mwd
-	#newImg=img.resize((int((mwd-widthImg*2)/widthImg),int((hg/wd)*((mwd-widthImg*2)/widthImg))),Image.ANTIALIAS)	
-	#canva.config(width=mwd)#height=mhg
-	#photo = ImageTk.PhotoImage(newImg)
-	#canva.image=photo
-	#imgCreated=canva.create_image(posX,posY,image=photo,anchor="nw")
-	#listImgOfCanvas.append(imgCreated)
-	#dicimg[photo] = photo
-		
-	#posX+=mwd/widthImg+2
-	#if posX>mwd :
-	#	posY+=((hg/wd)*((mwd-widthImg*2)/widthImg))+2
-	#	posX=0
-	#size=[]
-	#size.append(canva.winfo_width())
-	#size.append(canva.winfo_height())
-	#canva.saveAsImage('D:\\S4\\ProjetCheminDeFer\\UseCheminDeFer\\img.png',None,'PNG')
-	#im = Image.new('RGBA', size, (255, 255, 255, 255))
-	#draw=ImageDraw.Draw(im)
-	#canva.postscript(file='D:\\S4\\ProjetCheminDeFer\\UseCheminDeFer\\img.ps')
 	path='UseCheminDeFer/img.jpg'
 	new_im.save(path,'JPEG',quality=1000)
 	def callback():
@@ -107,33 +76,24 @@ def setCanvas(canva,dicimg,listImg,mwd,mhg,dm,nameProjet,rectFull,newWin):
 	if newWin is not None:
 		zoomImg=None
 	if zoomImg is None:
-		print("zoomImg is none")
 		canva.wm_state('iconic')
 		zoomImg=zi.Zoom_Advanced(canva,path,dm,getNumberImg(nameProjet),nameProjet)
-		#zoomImg.findAll()
 	else:
-		print("zoomImg is not none")
-		#zoomImg.findAll()
 		zoomImg.deleteAllButton()
-		#zoomImg.findAll()
 		canva.wm_state('iconic')
 		zoomImg==zi.Zoom_Advanced(canva,path,dm,getNumberImg(nameProjet),nameProjet)
-		#print("coucou")
-		#zoomImg.findAll()
-	
-	#time.sleep(3)
 	canva.wm_state('normal')
-	#new_im.show()
+
 
 #recuper le nombre de page
 def getNumberImg(nameProjet):
 		return len(os.listdir('DrawOnImage/finalResult/'+ nameProjet + '/emptyRect' ))#que les rectangles soit plein ou pas il y a antant de page
 
-
+#supprime les image du canvas
 def deleteCanvas(canva):
 	global listImgOfCanvas
 	for img in listImgOfCanvas:
 		canva.delete(img)
-
+#permet de zoomer et de se deplacer sur l'image
 def zoomImage(canva, dimention):
 	zoom_ad = Zoom_Advanced(canva,path,dimention)
